e08bd6710870a94cca9b8c72be7d634d5657f0a0
Daryl <67162425+resquator@users.noreply.github.com>
2022-11-05 17:02:30 +0100

    Add files via upload

---
+++ predictgame.py
@@ -0,0 +1,107 @@
+# load some libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+from sklearn.metrics import accuracy_score
+
+from datetime import datetime
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.pipeline import Pipeline
+import random
+from xgboost import XGBRegressor
+from sklearn.multioutput import MultiOutputRegressor
+import joblib
+
+import logging
+
+
+class PredictGame:
+    def __init__(self, game=None, knockout=False):
+        self.game=game
+        self.knockout = knockout
+        dateTimeObj = datetime.now()
+        timestampStr = dateTimeObj.strftime("%Y%m%d%H%M%S")
+        logging.basicConfig(filename=f'compute_wc_log/compute-{timestampStr}.log',  level=logging.DEBUG,
+            format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        #timestampStr
+                #self.features = ['1N2_1','1N2_N','1N2_2','home_team_rank_FIFA','home_team_trend','home_team_odd_trend','corr_score_home',
+                                            #'away_team_rank_FIFA','away_team_trend','away_team_odd_trend','corr_score_away']
+        #self.payout = game.loc[:,self.features].values.reshape(1,-1)
+        #print(ts.shape)
+        # define features
+
+    def predict(self):
+        
+        #load your model for further usage
+        feat = ['1N2_1','1N2_N','1N2_2','home_team_rank_FIFA','away_team_rank_FIFA','season','home_team','away_team']
+        #feat = ['1N2_1','1N2_N','1N2_2','home_team_rank_FIFA','away_team_rank_FIFA']
+        feat = ['1N2_1','1N2_N','1N2_2','home_team_rank_FIFA','away_team_rank_FIFA','season']
+
+        game_to_predict=self.game.loc[:,feat].values.reshape(1,-1)
+        game_to_predict=self.game.loc[:,feat]
+        c='1N2_1'
+        game_to_predict[c] = game_to_predict[c].astype(np.float64)
+        c='1N2_N'
+        game_to_predict[c] = game_to_predict[c].astype(np.float64)
+        c='1N2_2'
+        game_to_predict[c] = game_to_predict[c].astype(np.float64)
+        c='away_team_rank_FIFA'
+        game_to_predict[c] = game_to_predict[c].astype(np.float64)
+        c='home_team_rank_FIFA'
+        game_to_predict[c] = game_to_predict[c].astype(np.float64)
+        c='season'
+        game_to_predict[c] = game_to_predict[c].astype(np.int64)
+        
+        
+
+        """
+        for c in game_to_predict.columns.tolist():
+            game_to_predict[c] = game_to_predict[c].astype(np.float64)
+        """
+        #print(game_to_predict,'/n',game_to_predict.dtypes)
+        model = joblib.load("models/multi-regressor-score.pkl")
+        predict=model.predict(game_to_predict)
+        #print(f'predicted game {predict[0]}')       
+
+        h_score = predict[0][0] #random.randint(0, 5)
+        a_score = predict[0][1] #random.randint(0, 5)    
+        if self.knockout:
+            while h_score == a_score:
+                h_score = random.randint(0, 3)
+                a_score = random.randint(0, 3)    
+
+
+
+        if h_score == a_score:
+            pred1=1
+            result=1
+        elif h_score > a_score:
+            pred1=0
+            result=0
+        else:
+            pred1=2
+            result=2
+          
+        result_proba_1N2=[]
+        result_proba_12=[]
+        score = [h_score,a_score]
+        #print(score) 
+        res=[]
+        res.append([self.game.loc[:,'matchday'],self.game.loc[:,'home_team'],
+                    self.game.loc[:,'away_team'],self.game.loc[:,'correct_score'],pred1, 
+                    result, result_proba_1N2, result_proba_12,score])    
+        #print(res)
+        return res, res[0][0]
---
+++ worldcuptransformers.py
@@ -0,0 +1,44 @@
+import random
+import pandas as pd
+import numpy as np
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class CustomTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_name, additional_param = "SM"):  
+        #print('\n...intializing\n')
+        self.feature_name = feature_name
+        self.additional_param = additional_param
+        
+        rk = pd.read_csv('dataset/fifa_ranking-2021-05-27.csv')
+        feat = ['country_full','total_points','rank_change']
+        rk = rk[feat].groupby('country_full').mean().reset_index()      
+        self.rk = rk
+ 
+    def fit(self, X, y = None):
+        #print('\nfiting data...\n')
+        #print(f'\n \U0001f600  {self.additional_param}\n')
+        return self
+ 
+    def transform(self, X, y = None):
+        #print('\n...transforming data \n')
+        rk = self.rk
+        X_ = X.copy()
+        X_[self.feature_name] = (X_[self.feature_name])
+        X_[self.additional_param] = (X_[self.feature_name])
+        for idx, r in X_.iterrows():
+            ht=r['home_team']
+            wt=r['away_team']
+            try:
+                X_.loc[idx,'total_points']=float(rk.query('country_full == @ht')['total_points'])
+                X_.loc[idx,'rank_change']=float(rk.query('country_full == @ht')['rank_change'])
+                #print(float(rk.query('country_full == @ht ')['total_points']),float(rk.query('country_full == @ht ')['rank_change']))
+               
+                
+            except:
+                X_.loc[idx,'total_points']=0
+                X_.loc[idx,'rank_change']=0
+                
+                #print('error ranking...')
+            #print(f'Game {ht} Vs. {wt}')
+        return X_
